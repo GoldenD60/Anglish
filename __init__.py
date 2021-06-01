f87591ed599d9869ed5100e30f10bb7510d7a098
GoldenD60 <david@skillmanmedia.com>
2021-06-01 17:24:39 +0100

    Added command 'input'
    Now, users can interact with the program using the 'input' command.

--- __init__.py
+++ __init__.py
@@ -8,8 +8,10 @@ def open_file(filename):
 def lex(filecontents):
 	tok = ""
 	said = False
+	inputted = False
 	state = False
 	say = ""
+	inputs = ""
 	filecontents = list(filecontents)
 	for char in filecontents:
 		tok += char
@@ -18,18 +20,27 @@ def lex(filecontents):
 		if tok == "say" or tok == "Say":
 			tok = ""
 			said = True
+		if tok == "input" or tok == "Input" or tok == "ask" or tok == "Ask" or tok == "get" or tok == "Get":
+			tok = ""
+			inputted = True
 		if '"' in tok:
 			tok = ""
 			state = not state
 		if said and state:
 			say += char
-	say = say[1:]
-	print(say)
-
+		if inputted and state:
+			inputs += char
+		if state == False:
+			say = say[1:]
+			if say != '':
+				print(say)
+			if inputs != '':
+				input(inputs[1:])
+				inputs = ""
+				inputted = False
 
 def run():
 	data = open_file(argv[1])
 	lex(data)
-
 
 run()
